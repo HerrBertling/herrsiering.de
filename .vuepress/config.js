402274b17a2c1c5982d7b0251405ebf544d9c446
Markus Bertling <markus.bertling@jimdo.com>
2018-09-04 11:16:13 +0200

    feat: add favicon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,14 @@
 module.exports = {
   title: "Markus Siering",
-  description: "I help companies design and deliver digital products faster."
+  description: "I help companies design and deliver digital products faster.",
+  head: [
+    ["link", { rel: "icon", href: `/favicon-32x32.png` }],
+    ["meta", { name: "theme-color", content: "#1c71b1" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "white" }
+    ],
+    ["link", { rel: "apple-touch-icon", href: `/apple-touch-icon.png` }]
+  ]
 };
