61ccc295eedbd1426ae6c93ea8cb59bbd12f9a9a
Markus Siering <markus277@gmail.com>
2021-11-14 14:30:06 +0100

    fix: cleanup page generation + image import

--- src/components/ProfileImage.vue
+++ src/components/ProfileImage.vue
@@ -1,19 +1,13 @@
 <template>
   <picture class="md:col-span-3 md:col-start-6 mt-16 max-h-72 md:max-h-96">
     <source
-      srcset="
-        @/assets/images/markus-siering_2x.png 2x,
-        @/assets/images/markus-siering.png
-      "
-      type="image/png"
-    />
-    <source
-      srcset="
-        @/assets/images/markus-siering_2x.webp 2x,
-        @/assets/images/markus-siering.webp
-      "
-      type="image/webp"
-    />
+        :srcset="pngSrc"
+        type="image/png"
+      />
+      <source
+        :srcset="webpSrc"
+        type="image/webp"
+      />
     <img
       loading="lazy"
       class="
@@ -33,7 +27,23 @@
 </template>
 
 <script>
+import { computed } from "vue"
+import png1x from '@/assets/images/markus-siering.png'
+import png2x from '@/assets/images/markus-siering_2x.png'
+import webp1x from '@/assets/images/markus-siering.webp'
+import webp2x from '@/assets/images/markus-siering_2x.webp'
+
 export default {
   name: "ProfileImage",
+
+  setup() {
+    const webpSrc = computed(() => `${webp2x} 2x, ${webp1x}`)
+    const pngSrc = computed(() => `${png2x} 2x, ${png1x}`)
+
+    return {
+      pngSrc,
+      webpSrc,
+    }
+  },
 }
 </script>
