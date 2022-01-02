d463d7c37983ca7c252d0f592c6589eb98cffd21
Markus Siering <markus277@gmail.com>
2022-01-02 21:03:27 +0100

    fix(nav): make navigation work on mobile

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,29 +1,65 @@
 <template>
   <nav class="relative">
-    <NavButton @click="toggleNav" />
-    <ul
-      :class="[
-        'flex flex-col rounded-md overflow-hidden shadow-2xl absolute top-16 right-0 w-xs transform transition-transform duration-300 bg-white py-4 dark:(bg-gray-700) sm:(flex-row static w-auto shadow-none rounded-none bg-transparent justify-end items-center h-auto transform-none top-auto left-auto z-30) dark:sm:bg-transparent',
-        navExpanded && 'translate-x-0',
-        !navExpanded && 'translate-x-100vw',
-      ]"
+    <button
+      id="nav-toggle"
+      class="rounded-full px-4 py-2 flex items-center justify-center text-gray-700 dark:(text-white opacity-70) sm:hidden border border-transparent"
+      type="button"
     >
-      <NavItem
-        v-for="{ title, slug } in navigation"
-        :key="slug"
-        :title="title"
-        :slug="slug"
-      />
+      <span class="inline-block w-6 h-6 mr-2">
+        <svg
+          data-nav-visibility="open"
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h8m-8 6h16"
+          />
+        </svg>
+        <svg
+          data-nav-visibility="closed"
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 !hidden"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </span>
+      <span data-nav-visibility="open">Menu</span>
+      <span data-nav-visibility="closed" class="!hidden">Close</span>
+    </button>
+    <ul
+      id="navigation"
+      class="flex flex-col rounded-md overflow-hidden shadow-2xl absolute top-16 right-0 w-xs transform transition-transform translate-x-100vw duration-300 bg-white py-4 dark:(bg-gray-700) sm:(flex-row static w-auto shadow-none rounded-none bg-transparent justify-end items-center h-auto transform-none top-auto left-auto z-30) dark:sm:bg-transparent"
+    >
+      <li v-for="{ title, slug } in navigation" :key="slug">
+        <a
+          :href="slug"
+          class="inline-block text-gray-600 dark:text-gray-200 px-2 mx-2 py-2 border-transparent border-b-2"
+          exact-active-class="text-red-400 !dark:text-violet-700 border-pink-600 dark:border-purple-800"
+        >
+          {{ title }}
+        </a>
+      </li>
     </ul>
   </nav>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-
 const name = 'SiteNav'
 
-const navExpanded = ref(false)
 const navigation = [
   {
     title: 'Home',
@@ -46,8 +82,57 @@ const navigation = [
     slug: '/thoughts',
   },
 ]
+</script>
 
-const toggleNav = () => {
-  navExpanded.value = !navExpanded.value
+<script client:load lang="ts">
+import { ref, watch } from 'vue'
+
+export function onLoad() {
+  const openNav = ref(false)
+  watch(openNav, (v) => {
+    document
+      .getElementById('navigation')
+      ?.classList.toggle('!-translate-x-0', v)
+    document
+      .querySelectorAll<HTMLElement>('[data-nav-visibility]')
+      .forEach((el) => {
+        if (el.dataset.navVisibility === 'open') {
+          el.classList.toggle('!hidden', v)
+        }
+        if (el.dataset.navVisibility === 'closed') {
+          el.classList.toggle('!hidden', !v)
+        }
+      })
+  })
+  document.getElementById('nav-toggle').addEventListener('click', () => {
+    openNav.value = !openNav.value
+  })
+  document.querySelectorAll('#navigation > li > a').forEach((el) => {
+    el.addEventListener('click', () => {
+      openNav.value = !openNav.value
+    })
+  })
+
+  const path = window.location.pathname
+
+  document
+    .querySelectorAll<HTMLElement>('#navigation > li > a ')
+    .forEach((el) => {
+      if (el.getAttribute('href') === '/' && path === '/') {
+        el.classList.add(
+          'text-red-400',
+          'border-pink-600',
+          '!dark:text-violet-700',
+          'dark:border-purple-800'
+        )
+      } else if (el.getAttribute('href').startsWith(path) && path !== '/') {
+        el.classList.add(
+          'text-red-400',
+          'border-pink-600',
+          '!dark:text-violet-700',
+          'dark:border-purple-800'
+        )
+      }
+    })
 }
 </script>
