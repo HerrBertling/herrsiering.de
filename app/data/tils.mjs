bce6c5e1bd34dfa67f57e35e77b154ea2d68f416
Markus Siering <markus.siering@zolar.de>
2023-07-19 22:23:51 +0200

    feat: add scaffolding script for TIL, add entry about that

---
+++ app/data/tils.mjs
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Generating a small Node script with ChatGPT",
+    lastUpdated: "2023-07-19",
+    niceDate: "July 19, 2023",
+    href: "/til/23-07-19",
+  },
+  {
+    title: "Testing thrown redirects in React Router v6",
+    lastUpdated: "July 18, 2023",
+    niceDate: "July 18, 2023",
+    href: "/til/23-07-18",
+  },
+  {
+    title: "@testing-library/user-event clipboard",
+    lastUpdated: "July 10, 2023",
+    niceDate: "July 10, 2023",
+    href: "/til/23-07-10",
+  },
+];
