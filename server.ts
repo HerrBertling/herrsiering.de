055cea4d500bab3bc9546086b4b2bfb48ee510bf
Markus Siering <markus277@gmail.com>
2023-12-16 14:30:38 +0100

    fix: adjust to netlify remix adapter repo code

--- server.ts
+++ server.ts
@@ -1,10 +1,42 @@
-import { createRequestHandler } from "@netlify/remix-adapter";
-import * as build from "@remix-run/dev/server-build";
-import { installGlobals } from "@remix-run/node";
+import type { Context } from '@netlify/functions'
+import type { AppLoadContext, ServerBuild } from '@remix-run/node'
+import { createRequestHandler as createRemixRequestHandler } from '@remix-run/node'
 
-installGlobals();
+type LoadContext = AppLoadContext & Context
 
-export const handler = createRequestHandler({
+/**
+ * A function that returns the value to use as `context` in route `loader` and
+ * `action` functions.
+ *
+ * You can think of this as an escape hatch that allows you to pass
+ * environment/platform-specific values through to your loader/action.
+ */
+export type GetLoadContextFunction = (request: Request, context: Context) => Promise<LoadContext> | LoadContext
+
+export type RequestHandler = (request: Request, context: LoadContext) => Promise<Response | void>
+
+export function createRequestHandler({
   build,
-  mode: process.env.NODE_ENV,
-});
+  mode,
+  getLoadContext,
+}: {
+  build: ServerBuild
+  mode?: string
+  getLoadContext?: GetLoadContextFunction
+}): RequestHandler {
+  const remixHandler = createRemixRequestHandler(build, mode)
+
+  return async (request: Request, context: LoadContext): Promise<Response | void> => {
+    try {
+      const loadContext = (await getLoadContext?.(request, context)) || context
+
+      const response = await remixHandler(request, loadContext)
+
+      return response
+    } catch (error: unknown) {
+      console.error(error)
+
+      return new Response('Internal Error', { status: 500 })
+    }
+  }
+}
