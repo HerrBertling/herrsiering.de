20ffb472ed578be1f340a3fe3b487aa5e2a2fe05
Markus Siering <markus277@gmail.com>
2024-11-17 15:57:59 +0100

    chore: clear out old files

--- app/components/Buzzword.tsx
+++
@@ -1,7 +0,0 @@
-export default function Buzzword({ title }: { title: string }) {
-  return (
-    <span className="inline-block whitespace-nowrap rounded-md bg-slate-500 px-2 py-1 text-sm uppercase text-slate-50 dark:bg-slate-200 dark:text-slate-800">
-      {title}
-    </span>
-  );
-}
